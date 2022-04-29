e847e0fed696ebef6cf1b567d1be7f93731d4edd
Vlad Sokolov <vladqaz132435@ya.ru>
2022-04-29 19:54:58 +0300

    test

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express       = require('express');
+const serverless    = require('serverless');
+
+const app = express();
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.json({
+    'test': 'test123'
+  })
+});
+
+app.all('*', (req, res) => {
+    return res.status(404).send({
+        message: '404 page not found'
+    });
+});
+
+app.use('./netlify/functions', router)
+
+module.export.handler = serverless(app);
